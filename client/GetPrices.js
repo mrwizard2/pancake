0a1bd6f795cf407a7fe5c0c02b60d1e2a69382e9
mrwizard2 <4625443+mrwizard2@users.noreply.github.com>
2022-09-03 05:40:35 -0500

    2 files modified, 3 added

---
+++ client/GetPrices.js
@@ -0,0 +1,19 @@
+import { requireFontManifest } from 'next/dist/server/require';
+
+const ethers = require('ethers');
+
+const {
+  addressFactory,
+  addressFrom,
+  addressRouter,
+  addressTo,
+} = require('./AddressList');
+
+const { erc20ABI, factoryABI, routerABI, pairABI } = require('./ABIList');
+
+// Connect To A Standard Provider
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://bsc-dataseed.binance.org/'
+);
+
+console.log('Your provider is: ' + provider);
